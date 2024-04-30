37a9de7a7943d0246cc447904146a2bffd02cd37
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-04-30 22:15:50 +0900

    #1-domain
    주문(order)에 대한 도메인 작업

--- src/main/java/com/seul/orderservice/domain/OrderItem.java
+++ src/main/java/com/seul/orderservice/domain/OrderItem.java
@@ -1,4 +1,31 @@
 package com.seul.orderservice.domain;
 
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name = "order_items")
+@Getter
 public class OrderItem {
+
+    @Id
+    private int orderItemId;
+
+    @Column
+    private int productId;
+
+    @Column
+    private int orderId;
+
+    @Column
+    private int orderQuantity;
+
+    public OrderItem(int productId, int orderQuantity){
+        this.productId = productId;
+        this.orderQuantity = orderQuantity;
+    }
+
+
+
 }
