0682537438b3ce70a6ec8a0166732e47d9f7cc7e
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-05-01 00:55:41 +0900

    #1 - domain
    상품과 가게에 대한 도메인 클래스 정의

--- src/main/java/com/seul/orderservice/domain/Product.java
+++ src/main/java/com/seul/orderservice/domain/Product.java
@@ -1,4 +1,27 @@
 package com.seul.orderservice.domain;
 
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+
+@Table(name ="products")
+@Getter
 public class Product {
+    @Id
+    private String productId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private BigDecimal price;
+
 }
+
+
