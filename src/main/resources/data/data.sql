ff9a99a8136984453bb48b2458fa3df7cb99025b
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-05-04 01:01:58 +0900

    #1 - 데이터베이스 스키마 추카

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,5 @@
+-- products
+INSERT INTO products VALUES ('1','아이스 아메리카노','2샷 + 얼음 + 찬물', 3500.0);
+INSERT INTO products VALUES ('2','아메리카노','2샷 + 따뜻한 물', 3000.0);
+INSERT INTO products VALUES ('3','아이스 라떼','아이스 아메리카노 + 우유', 5500.0);
+INSERT INTO products VALUES ('4','라떼','아메라카노 + 따뜻한 우유', 5000.0);
